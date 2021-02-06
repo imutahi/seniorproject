20b5456df374c3c3001802c1370db7b05cf3bd44
root <root@WARREN-LAPTOP.localdomain>
2021-02-06 15:42:30 -0700

    Finished services and home index page along with navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root 'home#index'  
+  
+  get 'home/index'
   resources :firms
   resources :services
   resources :clients
-  root 'services#index'
 
-  resources :clients
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
