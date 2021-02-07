27bb45dcf9ff4a7e1f80f189931d881d50ee1918
Dominick Licciardi <dominick.nola@gmail.com>
2021-02-06 17:55:53 -0700

    first

--- app/models/firm.rb
+++ app/models/firm.rb
@@ -1,2 +1,11 @@
 class Firm < ApplicationRecord
+  validates :first_name, presence: true, length: { maximum: 60 }
+  validates :middle_name, length: { maximum: 60 }
+  validates :last_name, presence: true, length: { maximum: 60 }
+  validates :maiden_name, length: { maximum: 60 }
+  validates :dob, presence: true, length: { maximum: 40 }
+  validates :ssn, length: { maximum: 40 }
+  validates :dl_number, length: { maximum: 40 }
+  validates :dl_state, length: { maximum: 40 }
+  validates :notes, length: { maximum: 100 }
 end
