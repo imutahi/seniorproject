fb053736c246b9ee8ce64529e7d773ac7f291011
jose <josevwebpro@gmail.com>
2021-02-16 16:08:04 -0700

    add config.cache_store = :memory_store

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # prevent Cookie Overflow error when using Auth0
+  config.cache_store = :memory_store
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
