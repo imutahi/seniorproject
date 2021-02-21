dbc76a07f6cdeabf8ffd5b443c1f0a3e4ba9e48a
jose <josevwebpro@gmail.com>
2021-02-21 14:20:51 -0700

    add test to failure redirect to find welcome text

--- test/system/auth0_test.rb
+++ test/system/auth0_test.rb
@@ -1,6 +1,7 @@
 require "application_system_test_case"
 
 class Auth0Test < ApplicationSystemTestCase
+
   test "login" do
     visit root_url
     OmniAuth.config.test_mode = true
@@ -31,4 +32,10 @@ class Auth0Test < ApplicationSystemTestCase
     OmniAuth.config.mock_auth[:auth0] = :invalid_credentials
     click_on "My Login"
   end
+
+  test "should visit auth_failure redirect and find Welcome text" do
+    visit auth_failure_url
+    assert_text /Welcome/
+  end
+
 end
