68de13ee869c2eae2b8c06e1bd50f15e6df5eab3
WarrenJRamos <wjnramos@gmail.com>
2021-02-20 22:34:26 -0700

    created auth0_test to test valid login, invalid login, and logging out via auth0

---
+++ test/system/auth0_test.rb
@@ -0,0 +1,34 @@
+require "application_system_test_case"
+
+class Auth0Test < ApplicationSystemTestCase
+  test "login" do
+    visit root_url
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:auth0] = {
+      "provider"=> "auth0",
+      "uid"=> "google-oauth2|112708417615905138350",
+      "info"=> {
+        "email"=> "talontest7@gmail.com",
+        "name"=> "Talon Test"
+      },
+      "credentials" => {
+        "token" => "XWeawFFgIvBKi2TuhCm1blt_OPngjEKu",
+        "refresh_token" => nil,
+        "expires_at" => 1613871385
+      },
+    }
+    click_on "My Login"
+  end
+
+  test "logout" do
+    visit home_index_url
+    click_on "Logout"
+  end
+
+  test "login failure" do
+    visit root_url
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:auth0] = :invalid_credentials
+    click_on "My Login"
+  end
+end
