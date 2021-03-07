3eef1541d230a5f5141a87521bd7052879a989e0
WarrenJRamos <wjnramos@gmail.com>
2021-03-06 19:38:23 -0700

    display error descriptions code added to the production environment config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,10 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   #config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.file_watcher = ActiveSupport::FileUpdateChecker  # remove this and readd 61 when merge
+  OmniAuth.config.on_failure = Proc.new { |env|
+    message_key = env['omniauth.error.type']
+    error_description = Rack::Utils.escape(env['omniauth.error'].error_reason)
+    new_path = "#{env['SCRIPT_NAME']}#{OmniAuth.config.path_prefix}/failure?message=#{message_key}&error_description=#{error_description}"
+    Rack::Response.new(['302 Moved'], 302, 'Location' => new_path).finish
+  }
 end
