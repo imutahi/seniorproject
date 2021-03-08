cff1547df247bac016b4fb4e52dade81678dc613
jose <josevwebpro@gmail.com>
2021-03-07 22:35:04 -0700

    add has_and_belongs_to

--- app/models/firm.rb
+++ app/models/firm.rb
@@ -1,6 +1,6 @@
 class Firm < ApplicationRecord
   belongs_to :client, optional: true
-  has_many :services
+  has_and_belongs_to_many :services
 
   validates :first_name, presence: true, length: { maximum: 60 }
   validates :middle_name, length: { maximum: 60 }
