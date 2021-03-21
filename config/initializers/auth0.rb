47702fc84111f677d78fe8697bb1be22ff9db261
WarrenJRamos <wjnramos@gmail.com>
2021-03-21 11:33:52 -0600

    modified credentials configuration

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider(
       :auth0,
-      Rails.application.secrets.auth0_client_id,
-      Rails.application.secrets.auth0_client_secret,
-      Rails.application.secrets.auth0_domain,
+      Rails.application.credentials.development[:auth0][:auth0_client_id],
+      Rails.application.credentials.development[:auth0][:auth0_client_secret],
+      Rails.application.credentials.development[:auth0][:auth0_domain],
       callback_path: '/auth/auth0/callback',
       authorize_params: {
         scope: 'openid email profile'
