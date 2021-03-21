d57e1dd3f7a763885bd2922a2b673313e96c4b63
WarrenJRamos <wjnramos@gmail.com>
2021-03-21 11:48:13 -0600

    worked with Dominick and Jose to fix initializer for Omniauth middleware and credentials to reflect correctly

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider(
       :auth0,
-      Rails.application.credentials.development[:auth0][:auth0_client_id],
-      Rails.application.credentials.development[:auth0][:auth0_client_secret],
-      Rails.application.credentials.development[:auth0][:auth0_domain],
+      Rails.application.credentials.auth0_client_id,
+      Rails.application.credentials.auth0_client_secret,
+      Rails.application.credentials.auth0_domain,
       callback_path: '/auth/auth0/callback',
       authorize_params: {
         scope: 'openid email profile'
