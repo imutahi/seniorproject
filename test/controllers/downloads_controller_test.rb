d80917d6ef4ef2ce13e64d95ae11b45e15e32904
imutahi <--global>
2021-04-02 02:46:13 +0500

    Completed test cases for download request module

---
+++ test/controllers/downloads_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class DownloadsControllerTest < ActionDispatch::IntegrationTest
+
+	test 'it should get index' do
+		get downloads_url
+		assert_response :success
+	end
+
+	test "should get new" do
+	    get new_download_url
+	    assert_response :success
+	end
+
+	test "should create new" do
+	    post downloads_url, params:{ download:{ title: "Request", file:fixture_file_upload("/sample.pdf")} }
+	    assert_redirected_to downloads_url
+	 end
+	test "it should not create new" do
+	    post downloads_url, params:{ download:{ title: "Hey how are you? Have you completed your task"} }
+	    assert_redirected_to downloads_url
+	 end
+
+end
