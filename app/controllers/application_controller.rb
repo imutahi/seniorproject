c5fbb6d8e7a596f4fa58615caae427fdeb3a5c43
WarrenJRamos <wjnramos@gmail.com>
2021-04-05 21:11:03 -0600

    added a function to check if current user is an admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
-# :nocov:
 class ApplicationController < ActionController::Base
+    # FIX THIS IN THE FUTURE SO THE ADMIN IS QUERIED FROM THE ADMIN DATABASE INSTEAD OF A HARDCODED
+    # STRING
+    def curr_user_is_admin? 
+        if !session[:userinfo].nil? && session[:userinfo][:info][:email] == "talontest7@gmail.com"
+          return true
+        else
+          return false
+        end
+      end
 end
-# :nocov:
