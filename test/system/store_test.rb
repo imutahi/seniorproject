539c9a7b5ae29043f4e44db785895dcd34281d4b
jose <josevwebpro@gmail.com>
2021-04-12 18:52:40 -0600

    edit test name

--- test/system/store_test.rb
+++ test/system/store_test.rb
@@ -2,7 +2,7 @@ require "application_system_test_case"
 
 class StoreTest < ApplicationSystemTestCase
 
-  test "foo" do
+  test "visiting the checkout page" do
     visit store_index_url
     assert_text "Talon Agency Checkout"
   end
