112480342cac47e56b897462ba9393ba80bbdb1a
Grant Schumacher <grantschu99@gmail.com>
2021-04-19 11:01:06 -0600

    Migration for client email to download

--- db/migrate/20210419164818_add_client_id_to_downloads.rb
+++
@@ -1,5 +0,0 @@
-class AddClientIdToDownloads < ActiveRecord::Migration[6.0]
-  def change
-    add_column :downloads, :client_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_164818) do
+ActiveRecord::Schema.define(version: 2021_04_19_165927) do
 
   create_table "clients", force: :cascade do |t|
     t.string "client_name"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2021_04_19_164818) do
     t.string "file"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "client_id"
+    t.string "email"
   end
 
   create_table "firms", force: :cascade do |t|
