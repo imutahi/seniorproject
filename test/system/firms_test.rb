6ec69f3e631e334f5271334956a280e0eacb80f4
jose <josevwebpro@gmail.com>
2021-04-26 18:14:12 -0600

    increase code coverage

--- test/system/firms_test.rb
+++ test/system/firms_test.rb
@@ -4,11 +4,44 @@ class FirmsTest < ApplicationSystemTestCase
   setup do
     sign_in_system_admin
     @firm = firms(:one)
+    @client = clients(:one)
   end
 
   test "visiting the index" do
     visit firms_url
     assert_selector "h1", text: "Requests"
+  end
+
+  test "visit the index as a normal user" do
+    visit logout_url
+    sign_in_system_normal_without_client_application
+    visit clients_url
+    click_on "New Client"
+
+    fill_in "Address", with: @client.address
+    fill_in "City", with: @client.city
+    fill_in "Client", with: @client.client_id
+    fill_in "Client name", with: @client.client_name
+    fill_in "Company", with: @client.company
+    fill_in "Email", with: "noemail@test.com"
+    fill_in "Fax", with: @client.fax
+    fill_in "Notes", with: @client.notes
+    fill_in "Phone", with: @client.phone
+    fill_in "State", with: @client.state
+    #check "Activate" if @client.status
+    #fill_in "Total requests", with: @client.total_requests
+    fill_in "Zip", with: @client.zip
+    click_on "Save Client"
+
+    visit firms_url
+    assert_selector "h1", text: "Requests"
+    end
+
+    test "client is redirected if they don't have a client application" do
+    visit logout_url
+    sign_in_system_normal_without_client_applicationv2
+    visit firms_url
+    assert_selector "h1", text: "Clients"
   end
 
   test "creating a Firm" do
